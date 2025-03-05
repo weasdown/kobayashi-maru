707c7c387d381dc00973680e932c3b0ebca5e21e
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-05 14:28:07 +0000

    feat: add refresh button and refresh visuals via FutureBuilder

--- bridge_simulator/lib/src/presentation/websocket_viewer.dart
+++ bridge_simulator/lib/src/presentation/websocket_viewer.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:material_symbols_icons/symbols.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-class WebsocketViewer extends StatelessWidget {
+class WebsocketViewer extends StatefulWidget {
   const WebsocketViewer({super.key, required this.websocketUri});
 
   final Uri websocketUri;
 
+  @override
+  State<WebsocketViewer> createState() => _WebsocketViewerState();
+}
+
+class _WebsocketViewerState extends State<WebsocketViewer> {
   // TODO remove tempWebsocketUri and knownBadUri
   final bool deliberateFail = false;
+
+  void _refresh() => setState(() {});
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +49,7 @@ class WebsocketViewer extends StatelessWidget {
 
       if (error is WebSocketChannelException) {
         message =
-            'Connection could not be opened to WebSocket server at ${websocketUri.toString()}';
+            'Connection could not be opened to WebSocket server at ${widget.websocketUri.toString()}';
         return (message, text(message));
       } else {
         message = 'snapshot has an unrecognised error:\n\n$error';
@@ -50,46 +58,88 @@ class WebsocketViewer extends StatelessWidget {
     }
 
     final Uri knownBadUri = Uri(
-      scheme: websocketUri.scheme,
-      host: websocketUri.host,
+      scheme: widget.websocketUri.scheme,
+      host: widget.websocketUri.host,
       port: 1234,
     );
 
-    final Uri serverUri = deliberateFail ? knownBadUri : websocketUri;
+    final Uri serverUri = deliberateFail ? knownBadUri : widget.websocketUri;
 
     late final WebSocketChannel channel;
+
+    Future<WebSocketChannel> connectToChannel(Uri server) async =>
+        WebSocketChannel.connect(serverUri);
 
     channel = WebSocketChannel.connect(serverUri);
 
     return Scaffold(
-      appBar: AppBar(elevation: 20, title: Text('WebSocket Viewer')),
+      appBar: AppBar(
+        elevation: 20,
+        title: Text('WebSocket Viewer'),
+        actions: [IconButton(onPressed: _refresh, icon: Icon(Symbols.refresh))],
+      ),
       body: ListView(
         shrinkWrap: true,
         children: [
-          Gap(32),
-          FractionallySizedBox(
-            widthFactor: 0.75,
-            child: StreamBuilder(
-              stream: channel.stream,
-              initialData: 'Connecting to the WebSocket server...',
-              builder: (context, snapshot) {
-                late String message;
-                late Widget content;
+          FutureBuilder(
+            future: Future.wait<Object>([
+              Future<bool>.delayed(Duration(seconds: 2), () => true),
+              connectToChannel(serverUri),
+            ]),
+            builder: (context, fbSnapshot) {
+              fbNoneOrWaiting() => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Gap(50),
+                  Text(
+                    'Connecting to channel...',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  Gap(50),
+                  CircularProgressIndicator(),
+                ],
+              );
 
-                (message, content) =
-                    (snapshot.hasError)
-                        ? error(snapshot)
-                        : switch (snapshot.connectionState) {
-                          ConnectionState.waiting ||
-                          ConnectionState.none => loading(),
-                          ConnectionState.active || ConnectionState.done =>
-                            (message, content) = activeOrDone(snapshot),
-                        };
+              return switch (fbSnapshot.connectionState) {
+                ConnectionState.none ||
+                ConnectionState.waiting ||
+                ConnectionState.active => fbNoneOrWaiting(),
 
-                debugPrint(message);
-                return content;
-              },
-            ),
+                ConnectionState.done => ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Gap(32),
+                    FractionallySizedBox(
+                      widthFactor: 0.75,
+                      child: StreamBuilder(
+                        stream: channel.stream,
+                        initialData: 'Connecting to the WebSocket server...',
+                        builder: (context, snapshot) {
+                          late String message;
+                          late Widget content;
+
+                          (message, content) =
+                              (snapshot.hasError)
+                                  ? error(snapshot)
+                                  : switch (snapshot.connectionState) {
+                                    ConnectionState.waiting ||
+                                    ConnectionState.none => loading(),
+                                    ConnectionState.active ||
+                                    ConnectionState.done =>
+                                      (message, content) = activeOrDone(
+                                        snapshot,
+                                      ),
+                                  };
+
+                          debugPrint(message);
+                          return content;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              };
+            },
           ),
         ],
       ),
