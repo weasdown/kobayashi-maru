653354149651432af5d2ac24ffe23ff512f97fb1
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-05 10:20:40 +0000

    feat: add server/main.py

---
+++ server/main.py
@@ -0,0 +1,3 @@
+# This program runs a WebSocket server that acts as the central point of the Kobayashi Maru network.
+# It gathers information from and sends information to the various bridge stations (helm, tactical, etc).
+
