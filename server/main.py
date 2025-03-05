7559dd6e5f6d46fa25bd2627b17ee1bb5e3447b4
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-05 14:49:48 +0000

    feat: copy contents of sync_time.py (read only) into main.py

--- server/main.py
+++ server/main.py
@@ -1,3 +1,30 @@
+# !/usr/bin/env python
+
 # This program runs a WebSocket server that acts as the central point of the Kobayashi Maru network.
 # It gathers information from and sends information to the various bridge stations (helm, tactical, etc).
 
+import asyncio
+import datetime
+import random
+
+from websockets.asyncio.server import broadcast, serve
+
+
+async def noop(websocket):
+    await websocket.wait_closed()
+
+
+async def show_time(server):
+    while True:
+        message = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+        broadcast(server.connections, message)
+        await asyncio.sleep(random.random() * 2 + 1)
+
+
+async def main():
+    async with serve(noop, "localhost", 5678) as server:
+        await show_time(server)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
