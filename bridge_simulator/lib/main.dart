d8857456723c4bc7112dfab6e8398d3ec0174796
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-05 13:13:22 +0000

    fix: fix title

--- bridge_simulator/lib/main.dart
+++ bridge_simulator/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Bridge Simulator',
       theme: ThemeData(
         // This is the theme of your application.
         //
