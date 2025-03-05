32d5654191ee36c4df3fcfad79b6b027d9cb2827
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-05 20:13:53 +0000

    docs: export bridge_server.dart

--- bridge_simulator/lib/bridge_simulator.dart
+++ bridge_simulator/lib/bridge_simulator.dart
@@ -1,5 +1,6 @@
 /// A bridge simulator for Star Trek's Galaxy-class starships.
 library;
 
-export 'src/presentation/stations.dart';
+export 'src/data/websockets/bridge_server.dart';
 export 'src/presentation/buttons.dart';
+export 'src/presentation/stations.dart';
