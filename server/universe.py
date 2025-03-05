50b03223b97cc8aeb56478d1889ee005d17d3dd4
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-05 14:53:32 +0000

    feat: add universe.py with basic classes for universe modelling

---
+++ server/universe.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ShipPosition:
+    """A simple `(x, y)` representation of a ship's position."""
+    x: int
+    y: int
+
+
+class ShipState:
+    @dataclass
+    class Navigation:
+        """Data about the Enterprise's navigational state."""
+        position: ShipPosition = ShipPosition(0, 0)
+        in_neutral_zone: bool = False
+
+    @dataclass
+    class Structure:
+        """Data about the Enterprise's structure."""
+        hull_integrity: int = 100  # percent
+
+    def __init__(self):
+        """Model of the Enterprise."""
+        self.structure_data: ShipState.Structure = ShipState.Structure()
+
+        self.navigation_data: ShipState.Navigation = ShipState.Navigation()
+
+        self.shields_up: bool = False
+
+        self.weapons_locked: bool = False
+
+
+class Universe:
+    def __init__(self):
+        """Model of the universe in which the simulation takes place."""
+        self.ship_state: ShipState = ShipState()
+
+        self.klingon_positions: list[ShipPosition] = []  # No Klingons initially
