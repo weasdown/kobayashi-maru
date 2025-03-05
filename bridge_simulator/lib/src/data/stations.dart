d5a3dc8560558c2aa685c9066b3eb4318610c2d3
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-05 16:49:31 +0000

    feat: add stations.dart with BridgeStations enum

---
+++ bridge_simulator/lib/src/data/stations.dart
@@ -0,0 +1,13 @@
+/// The various stations and consoles around the Galaxy-class bridge.
+enum BridgeStations {
+  viewscreen,
+  ops,
+  conn,
+  captainChair,
+  tactical,
+  scienceI,
+  scienceII,
+  missionOps,
+  environment,
+  engineering,
+}
