211477e04c27a5270cb83e09d22c22b7a28db2d0
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-06 12:02:14 +0000

    fix: fix channel setting

--- bridge_simulator/lib/src/data/bridge_server.dart
+++ bridge_simulator/lib/src/data/bridge_server.dart
@@ -2,18 +2,20 @@ import 'dart:async';
 
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-final Uri webSocketServer = Uri(scheme: 'ws', host: 'localhost', port: 5678);
-
 /// A client-side interface to the WebSocket connection that enables data to be shared with the simulation server.
 interface class ServerInterface {
-  const ServerInterface({required this.channel});
+  const ServerInterface();
 
   /// The [WebSocketChannel] that data is sent across.
-  final WebSocketChannel channel;
+  WebSocketChannel? get channel => WebSocketChannel.connect(channelUri);
 
   /// Send a [message] to the WebSocket server.
-  void send(String message) => channel.sink.add(message);
+  void send(String message) => channel!.sink.add(message);
 
   /// The [Stream] that provides data from the server.
-  Stream get stream => channel.stream;
+  Stream get stream => channel!.stream;
+
+  /// The [Uri] that's used to access the [channel].
+  // TODO switch schema to (secure) 'wss'
+  static final channelUri = Uri(scheme: 'ws', host: 'localhost', port: 5678);
 }
