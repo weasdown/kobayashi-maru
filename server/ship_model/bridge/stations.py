7a5635aba91419416f95fe66eb5866d3f48db647
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-08 09:25:33 +0000

    feat: add stations.py with models of BridgeStation sub-types

---
+++ server/ship_model/bridge/stations.py
@@ -0,0 +1,41 @@
+import json
+
+from server.ship_model.bridge.bridge_station import BridgeStation
+
+
+class Tactical(BridgeStation):
+    def __init__(self, websocket):
+        """Models the Tactical bridge station."""
+        super().__init__(websocket)
+        self.phasers_firing: bool = False
+        self.torpedoes_firing: bool = False
+
+        self.torpedoes_remaining: int = 20
+
+    async def fire_phasers(self):
+        """Fires the ship's [phasers](https://memory-alpha.fandom.com/wiki/Phaser#Planetary_or_starship-mounted_phasers)."""
+        echo: str = 'Firing phasers as commanded!'
+        print(echo)
+        self.phasers_firing = True
+        await self.websocket.send(json.dumps({'firing_phasers': True}))
+        self.phasers_firing = False
+        await self.websocket.send(json.dumps({'firing_phasers': False}))
+
+    async def fire_torpedoes(self):
+        """Fires a [photon torpedo](https://memory-alpha.fandom.com/wiki/Photon_torpedo)."""
+        echo: str = 'Firing torpedoes as commanded!'
+        print(echo)
+        self.torpedoes_firing = True
+        if self.torpedoes_remaining > 0:
+            self.torpedoes_remaining -= 1
+            self.torpedoes_firing = False
+            await self.websocket.send(json.dumps({'torpedoes_remaining': self.torpedoes_remaining}))
+        else:
+            await self.websocket.send(json.dumps({'torpedoes_firing_failed': 'remaining == 0'}))
+
+
+class Viewscreen(BridgeStation):
+    def __init__(self, websocket):
+        """Models the Viewscreen at the front of the Bridge."""
+        super().__init__(websocket)
+        pass
