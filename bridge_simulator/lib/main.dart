6d1f00e52c99a638e968ebcbd8d7259ba38f76ca
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-08 11:54:45 +0000

    style: remove large theme comment

--- bridge_simulator/lib/main.dart
+++ bridge_simulator/lib/main.dart
@@ -25,7 +25,7 @@ void main() async {
   runApp(const Home(isServer: false));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key, required this.isServer});
 
   /// The [Bridge] instance used throughout the simulation.
@@ -35,30 +35,20 @@ class Home extends StatelessWidget {
   final bool isServer;
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bridge Simulator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: Scaffold(
-        body: mainBridge.tactical,
+        body: Home.mainBridge.tactical,
         // WebsocketViewer(websocketUri: ServerInterface.channelUri);),
       ),
     );
