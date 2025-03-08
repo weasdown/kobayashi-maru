009f506d81eb37343121e759140bb2bdf493fb69
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-08 17:56:53 +0000

    feat: add Bridge subtypes

--- bridge_simulator/lib/src/application/bridge.dart
+++ bridge_simulator/lib/src/application/bridge.dart
@@ -13,4 +13,45 @@ sealed class Bridge {
 
   late final Ship ship;
 }
+
+final class GalaxyClassBridge extends Bridge {
+  GalaxyClassBridge()
+    : super(
+        stations: [
+          viewscreen,
+          ops,
+          conn,
+          captainChair,
+          tactical,
+          scienceI,
+          scienceII,
+          missionOps,
+          environment,
+          engineering,
+        ],
+      );
+
+  static final ViewscreenBridgeStation viewscreen = BridgeStation.viewscreen;
+  static final OpsBridgeStation ops = BridgeStation.ops;
+  static final ConnBridgeStation conn = BridgeStation.conn;
+  static final CaptainChairBridgeStation captainChair =
+      BridgeStation.captainChair;
+  static final TacticalBridgeStation tactical = BridgeStation.tactical;
+  static final ScienceIBridgeStation scienceI = BridgeStation.scienceI;
+  static final ScienceIIBridgeStation scienceII = BridgeStation.scienceII;
+  static final MissionOpsBridgeStation missionOps = BridgeStation.missionOps;
+  static final EnvironmentBridgeStation environment = BridgeStation.environment;
+  static final EngineeringBridgeStation engineering = BridgeStation.engineering;
+}
+
+final class RomulanBridge extends Bridge {
+  RomulanBridge() : super(stations: []);
+}
+
+final class KlingonBridge extends Bridge {
+  KlingonBridge() : super(stations: []);
+}
+
+final class BorgBridge extends Bridge {
+  BorgBridge() : super(stations: []);
 }
