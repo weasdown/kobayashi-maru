1940dcebb15d11347ec497f0d70cf032d5620189
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-08 12:17:29 +0000

    feat: add attributes from WebSocketViewer

--- bridge_simulator/lib/main.dart
+++ bridge_simulator/lib/main.dart
@@ -4,6 +4,7 @@ library;
 import 'dart:io' show Platform;
 
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'src/application/bridge.dart';
@@ -38,13 +39,32 @@ class KobayashiMaru extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Home(isServer: false),
+      home: Home(isServer: false),
     );
   }
 }
 
 class Home extends StatefulWidget {
-  const Home({super.key, required this.isServer});
+  Home({super.key, required this.isServer}) : channelUri = defaultChannelUri;
+
+  Home.server({super.key, Uri? channelUri, required this.isServer})
+    : channelUri = channelUri ?? defaultChannelUri;
+
+  final Uri channelUri;
+
+  WebSocketChannel? connectToServer() =>
+  // TODO once connected, set initial simulation state from first stream data.
+  WebSocketChannel.connect(channelUri);
+
+  // TODO switch scheme to (secure) 'wss'
+  static final defaultChannelUri = Uri(
+    scheme: 'ws',
+    host: Platform.isWindows ? 'localhost' : '192.168.1.201',
+    port: 5678,
+  );
+
+  // TODO remove tempWebsocketUri and knownBadUri
+  final bool deliberateFail = false;
 
   // TODO implement usage of iServer: if true, acts as central simulation hub, else shows a user-selected BridgeStation.
   final bool isServer;
@@ -57,6 +77,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late WebSocketChannel channel;
+
+  // FIXME reconnect after channel close
+  void refresh() {
+    channel.sink.close();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
