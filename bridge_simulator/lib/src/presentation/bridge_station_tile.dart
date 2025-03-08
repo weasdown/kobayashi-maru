3fab60fe5d4c4e09ced17064e0775412de70233f
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-08 19:13:07 +0000

    feat: add title, children attributes and basic build method in BridgeStationTile

--- bridge_simulator/lib/src/presentation/bridge_station_tile.dart
+++ bridge_simulator/lib/src/presentation/bridge_station_tile.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 class BridgeStationTile extends StatelessWidget {
-  const BridgeStationTile({super.key});
+  const BridgeStationTile({
+    super.key,
+    required this.title,
+    required this.children,
+  });
+
+  /// A name that will appear at the top of the tile.
+  final String title;
+
+  /// The buttons to display within this tile.
+  final List<Widget> children;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: ListTile(
+        title: Text(title),
+        subtitle: ListView(shrinkWrap: true, children: children),
+      ),
+    );
   }
 }
