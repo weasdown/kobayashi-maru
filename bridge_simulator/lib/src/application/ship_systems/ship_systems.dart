f0c70eb440b3dfe9424e69792770bf0e1ce98e0a
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-08 17:49:54 +0000

    refactor: simplify ShipSystem arguments

--- bridge_simulator/lib/src/application/ship_systems/ship_systems.dart
+++ bridge_simulator/lib/src/application/ship_systems/ship_systems.dart
@@ -1,20 +1,15 @@
-import 'package:bridge_simulator/bridge_simulator.dart';
-
 import '../ship.dart';
 
 abstract class ShipSystem {
-  const ShipSystem({required this.ship, required this.station});
+  ShipSystem();
 
-  final BridgeStation station;
-
-  final Ship ship;
+  late final Ship ship;
 }
 
 final class Structure extends ShipSystem {
-  const Structure({required super.ship})
-    : super(station: BridgeStation.tactical);
+  Structure() : super();
 }
 
 final class Propulsion extends ShipSystem {
-  const Propulsion({required super.ship}) : super(station: BridgeStation.conn);
+  Propulsion() : super();
 }
