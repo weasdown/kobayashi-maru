f2da21095e5cb21cb9d03f1cf841210126897302
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-08 17:27:45 +0000

    feat: remove placeholderBuildMethod()

--- bridge_simulator/lib/src/presentation/stations.dart
+++ bridge_simulator/lib/src/presentation/stations.dart
@@ -92,14 +92,6 @@ sealed class BridgeStation extends StatefulWidget {
   static const EngineeringBridgeStation engineering =
       EngineeringBridgeStation();
 
-  // TODO remove placeholderBuildMethod() once all subtypes have implemented build()
-  Widget placeholderBuildMethod(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(name, style: Theme.of(context).textTheme.headlineMedium),
-      ),
-    );
-  }
 
   @override
   String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) => name;
@@ -182,42 +174,21 @@ class BridgeStationState extends State<BridgeStation> {
 /// Displays various views to the bridge crew, often of what's in front of the ship.
 final class ViewscreenBridgeStation extends BridgeStation {
   const ViewscreenBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Responsible for communications, scanning and course navigation.
 final class OpsBridgeStation extends BridgeStation {
   const OpsBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Responsible for piloting the ship.
 final class ConnBridgeStation extends BridgeStation {
   const ConnBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Provides key information to the ship's Captain.
 final class CaptainChairBridgeStation extends BridgeStation {
   const CaptainChairBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Responsible for weapons, shields, long-range sensors and communications (supporting the [OpsBridgeStation]).
@@ -269,41 +240,21 @@ class _TBSState extends BridgeStationState {
 /// Responsible for science investigations.
 final class ScienceIBridgeStation extends BridgeStation {
   const ScienceIBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Responsible for science investigations.
 final class ScienceIIBridgeStation extends BridgeStation {
   const ScienceIIBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Enables access to data about the ship's current mission (e.g. about nearby planets).
 final class MissionOpsBridgeStation extends BridgeStation {
   const MissionOpsBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Responsible for controlling the life support system to maintain a habitable and comfortable environment for the crew .
 final class EnvironmentBridgeStation extends BridgeStation {
   const EnvironmentBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
 
 /// Responsible for the ship's engines, transporters and other engineering systems.
@@ -311,9 +262,4 @@ final class EnvironmentBridgeStation extends BridgeStation {
 /// This is a backup to the consoles found in Main Engineering.
 final class EngineeringBridgeStation extends BridgeStation {
   const EngineeringBridgeStation({super.key}) : super._();
-
-  // @override
-  Widget build(BuildContext context) {
-    return placeholderBuildMethod(context);
-  }
 }
