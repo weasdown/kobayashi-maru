e55d024c0e9bcb044a281439e39639dadfbb1784
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-09 20:36:34 +0000

    docs: fix library docs, remove TODO

--- bridge_simulator/packages/dart_server/lib/dart_server.dart
+++ bridge_simulator/packages/dart_server/lib/dart_server.dart
@@ -1,9 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// WebSocket server for the Kobayashi Maru simulator.
 library;
 
 export 'src/dart_server.dart';
 export 'src/dart_server_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
