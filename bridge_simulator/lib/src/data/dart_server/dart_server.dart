02911c0b8cb19a214928c9e2f4d25afe1729d16f
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-09 17:23:35 +0000

    fix: fix warnings

--- bridge_simulator/lib/src/data/dart_server/dart_server.dart
+++ bridge_simulator/lib/src/data/dart_server/dart_server.dart
@@ -1,11 +1,12 @@
 /// A WebSocket server written in Dart.
 library;
 
+import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 /// Example server, copied from: https://pub.dev/packages/shelf_web_socket
-void main() {
+void serverExample() {
   var handler = webSocketHandler((webSocket, _) {
     webSocket.stream.listen((message) {
       webSocket.sink.add('echo $message');
@@ -13,6 +14,6 @@ void main() {
   });
 
   shelf_io.serve(handler, 'localhost', 8080).then((server) {
-    print('Serving at ws://${server.address.host}:${server.port}');
+    debugPrint('Serving at ws://${server.address.host}:${server.port}');
   });
 }
