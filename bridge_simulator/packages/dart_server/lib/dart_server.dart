3d210c3499540d1f8c3444013e6d9c73e3de3f5b
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-09 20:36:48 +0000

    docs: only export DataHandler from dart_server

--- bridge_simulator/packages/dart_server/lib/dart_server.dart
+++ bridge_simulator/packages/dart_server/lib/dart_server.dart
@@ -1,5 +1,5 @@
 /// WebSocket server for the Kobayashi Maru simulator.
 library;
 
-export 'src/dart_server.dart';
+export 'src/dart_server.dart' show DataHandler;
 export 'src/dart_server_base.dart';
