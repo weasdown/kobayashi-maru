b7b9ecec2a3b1a37582ee8398ab01cc2a027ce74
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-10 11:29:04 +0000

    docs: add doc for exitCleanly() function

--- lib/src/presentation/buttons.dart
+++ lib/src/presentation/buttons.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
+/// Exits the app with a standard exit code.
 void exitCleanly() {
   debugPrint('Exiting cleanly');
   exit(0);
