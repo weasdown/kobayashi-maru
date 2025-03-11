a4aae2f80070fbb196246c82cf0a11c7aea77bc0
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-11 23:08:05 +0000

    docs: add TODO to switch to just_audio package

--- lib/src/application/audio_player.dart
+++ lib/src/application/audio_player.dart
@@ -7,6 +7,7 @@ class SoundPlayer {
   void play(String path) {
     debugPrint('Playing $path');
 
+    // TODO convert to just_audio package that might be more reliable - see https://suragch.medium.com/playing-short-audio-clips-in-flutter-with-just-audio-3c80eb7eb6ea
     final player = AudioPlayer();
     player.play(AssetSource(path));
   }
