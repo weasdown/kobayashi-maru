72cd3693106e60cec90e778f2bb0c73a8af2e953
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-11 22:47:59 +0000

    feat: simplify audio.dart

--- lib/src/application/audio.dart
+++ lib/src/application/audio.dart
@@ -1,35 +1,13 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
+/// Simple example of playing audio.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // String soundFileRoot = 'sounds/files';
-
-  // String fileName = 'tng_phaser_clean.mp3';
   String fileName = 'ten_forward_doors.mp3';
 
-  // AudioCache.instance = AudioCache(prefix: '');
   final player = AudioPlayer();
 
-  // AssetSource source = AssetSource('sounds/files/weapons/$fileName');
-  // source.setOnPlayer(player);
-
-  AssetSource source2 = AssetSource(fileName);
-  // source2.setOnPlayer(player);
-
-  // await player.play(source);
-  await player.play(source2);
-
-  // print('source: ${player.source}');
-  // await player.setSource(AssetSource(fileName, mimeType: 'audio/mpeg'));
-
-  // print(AssetSource('assets').path);
-
-  // await player.setSource(
-  //   // AssetSource('$soundFileRoot/weapons/tng_phaser_clean.mp3'),
-  //   AssetSource('sounds/weapons/tng_phaser_clean.mp3'),
-  //
-  //   // 'assets/sounds/files/weapons/tng_phaser_clean.mp3'
-  // );
+  await player.play(AssetSource(fileName));
 }
