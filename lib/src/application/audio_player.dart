c7ae52f11170cc37b8f8d57cf8973c153f248507
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-12 22:29:33 +0000

    feat: configure SoundPlayer setup to reduce crashing from concurrent requests

--- lib/src/application/audio_player.dart
+++ lib/src/application/audio_player.dart
@@ -4,24 +4,28 @@ import 'package:minisound/engine_flutter.dart';
 
 /// Simple class to handle playing of audio, e.g. weapon sound effects.
 class SoundPlayer {
-  const SoundPlayer();
+  SoundPlayer() {
+    _setupEngine();
+  }
 
-  // FIXME if too many sounds are requested concurrently, the server crashes entirely. Perhaps add the sound requests to a queue then take them from the queue once the engine has initialised.
-  void play(String path) async {
-    debugPrint('Playing $path');
-
-    final engine = minisound.Engine();
-
+  void _setupEngine() async {
     // Engine initialization
     // You can pass `periodMs` as an argument, to change determines the latency (does not affect web). Can cause crackles if too low.
-    await engine.init();
+    await engine.init(20);
 
     // For web, this should be executed after the first user interaction due to browsers' autoplay policy
     await engine.start();
+  }
 
+  final minisound.Engine engine = minisound.Engine();
+
+  // FIXME if too many sounds are requested at once, the server crashes entirely, although it can now tolerate a lot of concurrent requests. Perhaps add the sound requests to a queue then take them from the queue once the engine has initialised.
+  void play(String path) async {
+    debugPrint('Loading $path');
     // Load the sound effect into the Engine.
     final LoadedSound sound = await engine.loadSoundAsset(path);
 
+    debugPrint('Playing $path');
     sound.play();
   }
 }
