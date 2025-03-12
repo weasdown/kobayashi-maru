4a3f694711afd0ad9f76184ffa2f100a5646a78d
weasdown <34220924+weasdown@users.noreply.github.com>
2025-03-12 20:53:25 +0000

    fix: convert SoundPlayer to use minisound for error-free audio

--- lib/src/application/audio_player.dart
+++ lib/src/application/audio_player.dart
@@ -1,14 +1,26 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart' show debugPrint;
+import 'package:minisound/engine.dart' as minisound;
+import 'package:minisound/engine_flutter.dart';
 
+/// Simple class to handle playing of audio, e.g. weapon sound effects.
 class SoundPlayer {
   const SoundPlayer();
 
-  void play(String path) {
+  void play(String path) async {
     debugPrint('Playing $path');
 
-    // TODO convert to just_audio package that might be more reliable - see https://suragch.medium.com/playing-short-audio-clips-in-flutter-with-just-audio-3c80eb7eb6ea
-    final player = AudioPlayer();
-    player.play(AssetSource(path));
+    final engine = minisound.Engine();
+
+    // Engine initialization
+    // You can pass `periodMs` as an argument, to change determines the latency (does not affect web). Can cause crackles if too low.
+    await engine.init();
+
+    // For web, this should be executed after the first user interaction due to browsers' autoplay policy
+    await engine.start();
+
+    // Load the sound effect into the Engine.
+    final LoadedSound sound = await engine.loadSoundAsset(path);
+
+    sound.play();
   }
 }
