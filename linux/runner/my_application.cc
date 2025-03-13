57aa460edef88d44e893e43cf56dad14f5466643
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2025-03-13 09:15:58 +0000

    fix: add linux/runner/my_application.cc
    Generated via Recreating project ....
    Resolving dependencies...
    Downloading packages...
    Got dependencies.
    Wrote 3 files.
    
    All done!
    You can find general documentation for Flutter at: https://docs.flutter.dev/
    Detailed API documentation is available at: https://api.flutter.dev/
    If you prefer video documentation, consider: https://www.youtube.com/c/flutterdev
    
    In order to run your application, type:
    
      $ cd .
      $ flutter run
    
    Your application code is in ./lib/main.dart.

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -5,9 +5,7 @@
 #include <gdk/gdkx.h>
 #endif
 
-#include "../../kobayashi_maru/packages/dart_server/windows/flutter/generated_plugin_registrant.h"
-
-#include <bitsdojo_window_linux/bitsdojo_window_plugin.h>
+#include "flutter/generated_plugin_registrant.h"
 
 struct _MyApplication {
   GtkApplication parent_instance;
@@ -49,8 +47,6 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "kobayashi_maru");
   }
 
-//  auto bdw = bitsdojo_window_from(window);            // <--- add this line
-//  bdw->setCustomFrame(true);                          // <-- add this line
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
 
